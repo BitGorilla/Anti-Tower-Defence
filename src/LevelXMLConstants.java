4a1e36c8cafadb8ac0d285e1c810b973e94abd31
Martin <martinsjolund@hotmail.com>
2018-11-28 10:25:01 +0100

    Created XMLConstants interface.

---
+++ src/LevelXMLConstants.java
@@ -0,0 +1,5 @@
+public interface LevelXMLConstants {
+    public static final String CREDITS = "credits";
+    public static final String TOWER = "tower";
+    public static final String TILE = "tile";
+}
