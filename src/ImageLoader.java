bceeea3d97cdcdc49b5b630323a21e3ec9d26155
Martin <martinsjolund@hotmail.com>
2018-12-07 09:07:17 +0100

    Created ImageLoader class.

--- src/ImageLoader.java
+++ src/ImageLoader.java
@@ -7,15 +7,15 @@ public class ImageLoader {
     private static ImageLoader loader;
     private HashMap<String, Image> imageStorage = new HashMap<>();
 
-    public Image getImage(String resourceName){
-        if(imageStorage.containsKey(resourceName)) {
-            return imageStorage.get(resourceName);
+    public Image getImage(String s, int scale){
+        if(imageStorage.containsKey(s)) {
+            return imageStorage.get(s);
         }
         else {
             try {
-                Image i = ImageIO.read(this.getClass().getResourceAsStream(resourceName));
-                i=i.getScaledInstance(50, 50, Image.SCALE_SMOOTH);
-                imageStorage.put(resourceName, i);
+                Image i = ImageIO.read(this.getClass().getResourceAsStream(s));
+                i=i.getScaledInstance(scale, scale, Image.SCALE_SMOOTH);
+                imageStorage.put(s, i);
                 return i;
             } catch (IOException e) {
                 return null;
