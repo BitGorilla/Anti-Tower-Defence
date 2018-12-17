b7405b89a27fb000d1960e5822f4f818040b2022
Martin <martinsjolund@hotmail.com>
2018-12-17 13:33:17 +0100

    Changed the info text.

--- src/gui/HelpDialog.java
+++ src/gui/HelpDialog.java
@@ -22,22 +22,25 @@ public class HelpDialog {
         textArea.setEditable(false);
 
         String s = "<h1>How to play</h1>"+
-                        "<p>The goal of the game is to get ten(10) creatures " +
-                "to the end of the path by not getting eliminated by the " +
-                "towers that shoot lasers.</p>" +
+                        "<p>The goal of the game is to get creatures to " +
+                "the end of the path with as much HP as possible. The " +
+                "towers along the path will shoot the creatures and you " +
+                "will have to get a certain amount of HP across the " +
+                "finish line to continue to the next level!</p>" +
                 "<p>To spawn creatures, press one of the creature buttons " +
                 "on the right side of the game window.</p>" +
                 "<p>A Portal creature can  spawn a portal on its position " +
                 "by pressing the spawn portal button while a portal creature" +
-                " is on the path. The portal becomes a shortcut for all " +
-                "creatures.</p><br>" +
+                " is on the path. The next portal will be placed " +
+                "automatically after a fixed period of time." +
+                " The portal becomes a shortcut for all creatures.</p><br>" +
                 "<b>Some path tiles are different:</b><br>" +
-                "<p>On some tiles places, creatues move slower or faster. " +
+                "<p>On some tiles places, creatures move slower or faster. " +
                 "There are also tiles where you can change the path the" +
                 "creatures are taking by pressing on the tile.</p>" +
                 "<b>If you win:</b><br>" +
-                "<p>Input your username to compare your score with other " +
-                "users!</p>";
+                "<p>Write your username to place your score among other " +
+                "users! You'll find all high scores in the info tab!</p>";
 
         textArea.setText(s);
         jdialog.add(textArea);
