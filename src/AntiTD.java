e9bfdd145835f1bf9249b6034e6d33eca61626b1
Martin <martinsjolund@hotmail.com>
2018-12-19 20:17:47 +0100

    Fixed .jar file can't read .xml-files.

--- src/AntiTD.java
+++ src/AntiTD.java
@@ -1,10 +1,7 @@
 import gui.Controller;
 import formatters.XMLReader;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
 
 /**
  * @author oi16ohn, id15msd
@@ -24,24 +21,38 @@ public class AntiTD {
         XMLReader reader;
         reader = new XMLReader(gameWidth);
 
-        try {
-            if (args.length == 0) {
-                reader.setSource(new FileInputStream(new File("src/xmlBuilder" +
-                        "/Maps/levels.xml")));
-            } else if (args.length == 1 && args[0].endsWith(".xml")) {
-                reader.setSource(new FileInputStream(new File(args[0])));
+
+        if (args.length == 0) {
+
+            InputStream is = AntiTD.class.getResourceAsStream(
+                    "/xmlBuilder/Maps/levels.xml");
+            if (null != is) {
+                reader.setSource(is);
             } else {
-                System.err.println("The program only takes one argument, an .xml " +
-                        "file containing the maps.\nIf no arguments are given " +
-                        "the " +
-                        "default maps are run.");
+                System.err.println("Couldn't find that file!");
                 System.exit(1);
+
             }
-        }
-        catch (FileNotFoundException e) {
-            System.err.println("Could not find that file!");
+
+        } else if (args.length == 1 && args[0].endsWith(".xml")) {
+
+            InputStream is = AntiTD.class.getResourceAsStream(
+                    "/xmlBuilder/Maps/"+args[0]);
+            if (null != is) {
+                reader.setSource(is);
+            } else {
+                System.err.println("Couldn't find that file!");
+                System.exit(1);
+
+            }
+        } else {
+            System.err.println("The program only takes one argument, an .xml " +
+                    "file containing the maps.\nIf no arguments are given " +
+                    "the " +
+                    "default maps are run.");
             System.exit(1);
         }
+
         new Controller(reader.getMaps(), gameWidth, reader.getWidth());
     }
 
